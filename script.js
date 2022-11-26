43f310d8d02c239007cbe5a2d31b020910d0b62b
Prantik Saha <33820556+prantiksaha47@users.noreply.github.com>
2022-11-26 20:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 
 function calc(){
   
-    int age = 21;
+    static age =21;
 
       if (!isNaN(age)){
-        var string = document.createElement("div");
+        static string = document.createElement("div");
         string.setAttribute('class', 'stringCSS');
         string.innerHTML ="You've been alive for : ";   
         document.body.appendChild(string);
 
-        var daysOutput = document.createElement("div");
+        static daysOutput = document.createElement("div");
         days=365*age;
         daysOutput.setAttribute('class', 'outputCSS');
         daysOutput.innerHTML = days+" days";   
         document.body.appendChild(daysOutput);
 
-        var hoursOutput = document.createElement("div");
+        static hoursOutput = document.createElement("div");
         hours=8766*age;
         hoursOutput.setAttribute('class', 'outputCSS');
         hoursOutput.innerHTML = hours+" hours";   
         document.body.appendChild(hoursOutput);
         
 
-        var secondsOutput = document.createElement("div");
+        static secondsOutput = document.createElement("div");
         seconds=31536000*age;
         secondsOutput.setAttribute('class', 'outputCSS');
         secondsOutput.innerHTML = seconds+"+ seconds";
